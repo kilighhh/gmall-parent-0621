53442775f08a5c151f13d0518c5409ea21689cc8
zong-guibing <1142732681@qq.com>
2020-12-02 22:56:45 +0800

    商品详情的编写查询

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/mapper/BaseCategoryViewMapper.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.product.mapper;
+
+import com.atguigu.gmall.model.product.BaseCategoryView;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author Kilig Zong
+ * @Date 2020/12/2 20:03
+ * @Version 1.0
+ */
+@Mapper
+public interface BaseCategoryViewMapper extends BaseMapper<BaseCategoryView> {
+}
